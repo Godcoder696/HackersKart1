ab8bc7ef30700aca658ec6f013c2b6c435b3db86
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2023-09-21 00:28:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
-import './App.css';
+import homepage from './homepage.css';
+import blinkit_final from './blinkit_final.png'
+import plus from './plus.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <>
+        <div className="pageBody">  
+          <div className="leftSection">
+            <img src={blinkit_final} alt="" className="offers"/>
+            <div className="leftWrapper">
+              <div className="suggestion">
+                TOP PICKS FOR YOU
+              </div>
+              <div className="allSuggestions">
+
+              </div>
+            </div>
+          </div>
+          <div className="rightSection">
+            <div className="myCart">
+              <div className="cartWrapper">
+                <div className="rightHeading">
+                  YOUR CART
+                </div>
+                <div className="add">
+                  <img src={plus} alt="" className="plusImg"/>
+                  <div className="addItemHeader">ADD ITEM</div>
+                </div>
+                <div className="cartItems">
+
+                </div>
+                <div className="totalBill">
+                  <div className="total">
+                    TOTAL:
+                  </div>
+                  <div className="bill">
+                    Rs. 40
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="checkOutButton">
+              CHECK OUT
+            </div>
+          </div>
+        </div>
+      </>
+    </>
   );
 }
 
